6679afaec366cf23a8f0ec8eba275bae8f6eca9d
UGcraftist <UGcraftism@outlook.com>
2025-09-13 01:54:27 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from ollama import chat
+from ollama import ChatResponse
+import multiprocessing as mp
+
+
+
+response: ChatResponse = chat(model='gemma3', messages=[
+  {
+    'role': 'user',
+    'content': 'Why is the sky blue?',
+  },
+])
+print(response['message']['content'])
+# or access fields directly from the response object
+print(response.message.content)
